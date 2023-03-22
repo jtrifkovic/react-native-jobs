4c67523bc82146fa48a5678deac75031df6a2531
jtrifkovic <jowantrifkovic@gmail.com>
2023-03-22 13:02:43 +0100

    setuping project and additional files

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import styles from './welcome.style'
+import { icons, SIZES } from "../../../constants"
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
+
+export default Welcome
